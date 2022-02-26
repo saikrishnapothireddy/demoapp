b353769bdcfc35ae8b3922fab82289b85afb0cd7
saikrishna <saikrishnapothireddy@gmail.com>
2022-02-26 20:18:23 +0530

    relationship changes bugs

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,18 +4,30 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
+    redirect_to @user
+    # respond_to do |format|
+    #   format.html { redirect_to @user }
+    #   format.js
+    # end
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
+    redirect_to @user
+    # respond_to do |format|
+    #   format.html { redirect_to @user }
+    #   format.js
+    # end
   end
+
+  private
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
